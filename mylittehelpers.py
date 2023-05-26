f6dcd203e585f4b575f8f051b9f8537daf44b833
Gupitc <117738424+Gupitc@users.noreply.github.com>
2023-05-26 12:34:25 +0200

    Add files via upload

---
+++ mylittehelpers.py
@@ -0,0 +1,9 @@
+# Python Lib all meiner Klassen
+
+# by Vincent Kochinke; 25. Mai 2023
+
+# Dateipfade für 
+PRICING_DATA_DIR = r'C:\Users\vince\OneDrive\Programmieren\GitHub\Python\ML\Datasets\tankerkoenig\Data'
+TARGET_PRICING_DATA_PATH  = r'C:\Users\vince\OneDrive\Programmieren\GitHub\Python_Private\Datasets\tankerkoenig\target_pricing_data.csv'
+TARGET_STATIONS_PATH = r'C:\Users\vince\OneDrive\Programmieren\GitHub\Python_Private\Datasets\tankerkoenig\target_stations.csv'
+DATE_BY_STATION_PATH = r'C:\Users\vince\OneDrive\Programmieren\GitHub\Python_Private\Datasets\tankerkoenig\Data_by_Station'
